c228067f628315990630b874945466998a5436fb
Jina Pro <jina.pro.23@gmail.com>
2024-04-02 14:02:08 +0000

    fix: default theme light

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
